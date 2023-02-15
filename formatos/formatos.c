967b1a5456f3e0e69ce4d60fe61cfd83de6f877f
uevertonpassos <uevertonpassos96@gmail.com>
2023-02-15 18:21:03 -0300

    vamos ueverton, vc consegue

---
+++ formatos/formatos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("the number is %d\n", 14);
+    printf("the other one is %f\n", 15.12);
+    printf("batata? %s \n", "obviamente batata");
+    printf("da pra colocar com duas casas %.2f.\n", 12.32343);
+}
+
