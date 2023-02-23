f4794e6e1b1db23178cdbe4717855ab757a34f30
Ueverton Passos <uevertonpassos@gmail.com>
2023-02-23 18:14:07 -0300

    exercicios

---
+++ exercicios/ex011.c
@@ -0,0 +1,8 @@
+/*Faça um Programa que mostre a mensagem "Alo mundo" na tela. */
+
+#include <stdio.h>
+
+void main(){
+    printf("Olá, mundo!\n");
+    //return 0;
+}
---
+++ exercicios/ex012.c
@@ -0,0 +1,12 @@
+/*Faça um Programa que peça um número e então mostre a mensagem
+ O número informado foi [número]. */
+
+ #include <stdio.h>
+
+ void main(){
+    int num;
+    printf("Digite um numero:");
+    scanf("%d", &num);
+    printf("O número informado foi: %d\n", num);
+
+ }
