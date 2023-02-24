ce42127c4eccbc633d9ad3f8d74a2996df3353e8
Ueverton Passos <uevertonpassos@gmail.com>
2023-02-23 21:10:47 -0300

    mais exercicios

---
+++ exercicios/ex022.c
@@ -0,0 +1,15 @@
+/* Tendo como dados de entrada a altura de uma pessoa, 
+construa um algoritmo que calcule seu peso ideal, 
+usando a seguinte fórmula: (72.7*altura) - 58 */ 
+
+#include <stdio.h>
+
+void main(){
+    float altura, pesoideal;
+
+    printf("Digite a sua altura");
+    scanf("%f", &altura);
+    pesoideal = (72.7*altura) - 58;
+    printf("Seu peso ideal será %.2f\n", pesoideal);
+
+}
